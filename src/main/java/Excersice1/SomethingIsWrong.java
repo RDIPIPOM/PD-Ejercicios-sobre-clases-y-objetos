942a5e153edb6340ed2d267adc66c2910ab34b10
RDIPIPOM <ezamora2@ucol.mx>
2021-02-27 19:58:25 -0600

    First update

---
+++ src/main/java/Excersice1/SomethingIsWrong.java
@@ -0,0 +1,10 @@
+//package Excersice1;
+//
+//public class SomethingIsWrong {
+//    public static void main(String[] args) {
+//        Rectangle myRect; //No es la forma adecuada de declarar un objeto y además la clase no fue importada anteriormente
+//        myRect.width = 40;
+//        myRect.height = 50;
+//        System.out.println("myRect's area is " + myRect.area());
+//    }
+//}
---
+++ src/main/java/Excersice2/Main.java
@@ -0,0 +1,17 @@
+package Excersice2;
+
+public class Main {
+    public static void Main(String[] args){
+        IdentifyMyParts a = new IdentifyMyParts();
+        IdentifyMyParts b = new IdentifyMyParts();
+        a.y = 5;
+        b.y = 6;
+        a.x = 1;
+        b.x = 2;
+        System.out.println("a.y = " + a.y);
+        System.out.println("b.y = " + b.y);
+        System.out.println("a.x = " + a.x);
+        System.out.println("b.x = " + b.x);
+        System.out.println("IdentifyMyParts.x = " + IdentifyMyParts.x);
+    }
+}
